922e0cb288fb6c0855d8f0fc71450a4299fa924f
junga8 <pthapa1@students.towson.edu>
2020-11-24 20:53:12 -0500

    Redoing the broken minus test

--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -9,13 +9,29 @@ public class SimpleCalculatorTest {
 	@Test
 	public void testAdd() {
 		SimpleCalculator calc = new SimpleCalculator();
-		assertEquals(calc.add(1, 1), 2);
+		assertEquals(calc.add(1, 1), 0);
 	}
 
+	@Test
 	public void testminus(){
 
 		SimpleCalculator calc = new SimpleCalculator();
 		assertEquals(calc.minus(5, 3), 2);
+
+	}
+	@Test
+	public void testmultiply(){
+
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.multiply(5, 3), 15);
+
+	}
+	@Test
+	public void testdivide(){
+
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.divide(4, 2), 2);
+		//assertEquals(calc.divide(4, 0),"Error");
 
 	}
 
